43a0625abe294acee3186f3c88776a16d7c61f12
Jarwin <jarwbiz@gmail.com>
2025-08-01 18:20:14 +0200

    New comments added.

--- Py-uppgifter/övning_8.8.py
+++ Py-uppgifter/övning_8.8.py
@@ -1,17 +1,31 @@
+# Importerar factorial från math.
 from math import factorial
 
+# Användaren anger det tal som e ska upphöjas till och hur många termer som ska ingå i serien.
 x=float(input("Ange ett tal: "))
 n=int(input("Ange antal termer: "))
 
 def Maclaurin(x, n):
-
+    
+    # Resultat initieras till 0.
     resultat = 0
     
+    # För varje tal k i n, bildas en term i serien med hjälp av k.
     for k in range(n):
         term = (x ** k) / factorial(k)
+
+        # Om denna term är större en -10^7 adderas den till resultatet, annars inte,
+        # eftersom den då inte skulle ha någon större inverkan på resultatet.
         if term > 0.0000001:
             resultat += term
-            
+
+    # Resultatet av beräkningen returneras.        
     return resultat
 
-print("Maclaurin-serien för e^x med", n, "termer är:", Maclaurin(x, n))
+# Kollar om n är mindre än 1, då skrivs ett felmeddelande ut.
+if n<1:
+    print("Åtminstone en term behövs!")
+
+# Annars kallas funktionen och resultatet skrivs ut.    
+else:
+    print("Maclaurin-serien för e^x med", n, "termer är:", Maclaurin(x, n))
--- Py-uppgifter/övning_8.9.py
+++ Py-uppgifter/övning_8.9.py
@@ -1,19 +1,26 @@
+# Användaren anger ett tal x.
 x=float(input("Ange x: "))
 
 def Newtons_metod(x):
     
-    if x < 0:
-        raise ValueError("Kan inte beräkna roten ur negativa tal.")
-    
     gissning = x / 2  # Startgissning
     
+    # Medan loopen inte bryts (vilket den gör då skillnaden mellan gissningen och den nya
+    # gissningen är tillräckligt liten), används Newtons metod för att komma allt närmare
+    # kvadratroten ur x.
     while True:
         ny_gissning = (gissning + x / gissning) / 2
         if abs(ny_gissning - gissning) < 0.000001:
             break
         gissning = ny_gissning
     
+    # Gissningen returneras.
     return gissning
+ 
+# Om talet är negativt skrivs ett felmeddelande ut.
+if x < 0:
+    print("Kan inte beräkna roten ur negativa tal.")
 
-print("Roten av x är circa: ", Newtons_metod(x),)
-
+# Annars kallas funktionen och resultatet skrivs ut.
+else:
+    print("Roten av x är circa: ", Newtons_metod(x),)
